ecfcb86875756832c7b676d7a88095f69254be0f
Daniel Anyetewen <danyetewen@gmail.com>
2025-08-17 22:30:29 +0000

    Correction of Full description

--- task_4.sql
+++ task_4.sql
@@ -1,2 +1,8 @@
--- task_4.sql
-SHOW CREATE TABLE books;
+-- Switch to the alx_book_store database
+USE alx_book_store;
+
+-- Print full description of the books table
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'books'
+  AND TABLE_SCHEMA = 'alx_book_store';
