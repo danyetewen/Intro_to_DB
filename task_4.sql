949bfe5e3fac52149f8a805d04a4ce0d93c08978
Daniel Anyetewen <danyetewen@gmail.com>
2025-08-17 22:34:25 +0000

    second correction of Full description

--- task_4.sql
+++ task_4.sql
@@ -1,8 +1,8 @@
 -- Switch to the alx_book_store database
 USE alx_book_store;
 
--- Print full description of the books table
+-- Print full description of the Books table
 SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
 FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME = 'books'
+WHERE TABLE_NAME = 'Books'
   AND TABLE_SCHEMA = 'alx_book_store';
