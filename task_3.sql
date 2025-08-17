7b47ad0e8459e3f50e6e70fcd42120e3236cff24
Daniel Anyetewen <danyetewen@gmail.com>
2025-08-17 22:26:25 +0000

    second correction of  List all the tables created

--- task_3.sql
+++ task_3.sql
@@ -1,12 +1,5 @@
--- Switch to the correct database
+-- Switch to the alx_book_store database
 USE alx_book_store;
 
--- Create the table orders
-CREATE TABLE orders (
-    order_id INT PRIMARY KEY AUTO_INCREMENT,
-    customer_id INT NOT NULL,
-    order_date DATE NOT NULL,
-    total_amount DECIMAL(10,2) NOT NULL,
-    FOREIGN KEY (customer_id) REFERENCES customer(customer_id)
-);
-
+-- List all tables in the database
+SHOW TABLES;
